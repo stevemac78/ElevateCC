cfc441f3cfe01aa97974a1344d6ffb3a7066764a
stevemac78 <101868933+stevemac78@users.noreply.github.com>
2023-10-30 11:40:46 +0000

    Update script2.js

--- src/script2.js
+++ src/script2.js
@@ -531,7 +531,7 @@ function populateCalls (cdata) {
                 tr.appendChild(td2);
                  
                 const td3 = document.createElement("td");
-                td3.textContent = row.GeneralInfo.Data;
+                td3.textContent = row.Checkpoints.Data;
                 tr.appendChild(td3);
 
 
